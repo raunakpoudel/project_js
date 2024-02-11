a3c6db032949de26e69e00d9dc998629bd56cfeb
raunakpoudel <raunakpoudel8@gmail.com>
2024-02-11 07:44:54 +0545

    modified promises and created promise2

--- promises.js
+++ promises.js
@@ -17,111 +17,111 @@
 
 
 
-// const promisone =new Promise((resolve , reject )=>{
-//     setTimeout(()=>{
-// console.log("resolved")
+const promisone =new Promise((resolve , reject )=>{
+    setTimeout(()=>{
+console.log("resolved")
         
-//  resolve();
-//     },2000)
-// });
+ resolve();
+    },2000)
+});
 
-// promisone.then(()=>{
-//     console.log("Promises")
-// })
+promisone.then(()=>{
+    console.log("Promises")
+})
 
 
 
-// new Promise((resolve,reject)=>{
-//    setTimeout(()=>{
-//     console.log("promisesTwo setTimeout executed ")
-// resolve()
-//    },2000) 
-// }).then(
-//     console.log("PromiseTwo resolved")
-// );
+new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+    console.log("promisesTwo setTimeout executed ")
+resolve()
+   },2000) 
+}).then(
+    console.log("PromiseTwo resolved")
+);
 
 
 //--------------------------------------------------------------------------------------------------------------------
 // now in this example we use the data transfering as shown in the example 
 
 
-// const promisefour =new Promise((resolve,reject)=>{
-//     setTimeout(()=>{
-//         console.log("Resolved")
-//     resolve({username:"Jeevan Poudel",Email:"example@gmail.com"})
-//     },1000)
-// });
+const promisefour =new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("Resolved")
+    resolve({username:"Jeevan Poudel",Email:"example@gmail.com"})
+    },1000)
+});
 
-// promisefour.then(
-//     (user)=>{
-//         console.log(user);
-//         return user;
-//     }
-// ).then(
-//     (user)=>{
-// console.log(user.username);
-//     }
-// )
+promisefour.then(
+    (user)=>{
+        console.log(user);
+        return user;
+    }
+).then(
+    (user)=>{
+console.log(user.username);
+    }
+)
 
 
 // ---------------------------------------------------------------------------------------------------------
 // now let's try rejection too
-// const promisefive =new Promise((resolve,reject)=>{
-//     setTimeout( ()=>{
-//             error =true
-//             if(!error){
-//                 resolve();
-//             }
-//             else{
-//                 reject();
-//             }
-//         },1000)
-//     }
-// );
+const promisefive =new Promise((resolve,reject)=>{
+    setTimeout( ()=>{
+            error =true
+            if(!error){
+                resolve();
+            }
+            else{
+                reject();
+            }
+        },1000)
+    }
+);
 
-// promisefive.then(
-// ()=>{
-//     console.log("True");
+promisefive.then(
+()=>{
+    console.log("True");
 
-// })
-// promisefive.catch(
-// ()=>{
-//     console.log("False");
+})
+promisefive.catch(
+()=>{
+    console.log("False");
 
-// })
+})
 
 // -------------------------------------------------------------------------------------------------------------------------------------------
 // async and await in promises
 
 
-// const promisesix =  new promise((resolve,reject)=>{
-//     setTimeout(()=>{
-//         const error = true;
-//         if(!error){
-//             resolve(
-//                 ()=>{
-//                     console.log("No error")
-//                 }
-//             )
-//         }
-//         else{
-//             reject(()=>{
-//                 console.log("Error present")
-//             })
-//         }
-//     },2000)
-// })
+const promisesix =  new promise((resolve,reject)=>{
+    setTimeout(()=>{
+        const error = true;
+        if(!error){
+            resolve(
+                ()=>{
+                    console.log("No error")
+                }
+            )
+        }
+        else{
+            reject(()=>{
+                console.log("Error present")
+            })
+        }
+    },2000)
+})
 
-// async function compromise(){
-//     try {
-//         const a =await promisesix(
-//             console.log(a)
+async function compromise(){
+    try {
+        const a =await promisesix(
+            console.log(a)
 
-//         )
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 // -------------------------------------------------------------------------------------------------------------------------------------------
 // fatching data using json 
@@ -139,3 +139,6 @@ fetch('https://jsonplaceholder.typicode.com/users')
 .catch((error)=>{
     console.log(error)
 })
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+
